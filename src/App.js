f0027f46f2f46e75131891d565e409841109e0a9
emiandd <emilandresaravena@icloud.com>
2022-08-06 19:05:42 -0400

    routes: all pages added with respective route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Contact from './pages/Contact.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/portfolio' element={<Portfolio />} /> 
+        <Route path='/about-me' element={<AboutMe />} /> 
+        <Route path='/contact' element={<Contact />} /> 
+      </Routes>
     </div>
   );
 }
